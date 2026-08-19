98f98ee2f7e3cd70c2e161002cf00ccba036b7e8
Thomas Tornevall <thorne@tornevall.net>
2026-08-19 10:41:05 +0200

    Clean module state on uninstall

--- uninstall.php
+++ uninstall.php
@@ -9,4 +9,6 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
+wp_clear_scheduled_hook( 'ttfw_dyndns_update_event' );
 delete_option( 'ttfw_options' );
+delete_option( 'ttfw_dyndns_status' );
