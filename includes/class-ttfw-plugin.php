30190850d3450bfdb937117ff5fa91cba6fc5814
Thomas Tornevall <thorne@tornevall.net>
2026-08-21 13:10:35 +0200

    Initialize Tools account admin UI

--- includes/class-ttfw-plugin.php
+++ includes/class-ttfw-plugin.php
@@ -21,6 +21,7 @@ class TTFW_Plugin {
 	public static function init() {
 		TTFW_Settings::init();
 		TTFW_Tools_Connection::init();
+		TTFW_Tools_Connection_Admin::init();
 		TTFW_Dynamic_DNS_Module::init();
 		TTFW_Guestbook_Settings::init();
 		TTFW_Guestbook::init();
