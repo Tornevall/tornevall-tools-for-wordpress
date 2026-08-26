84edfcbc03d2fdb7bff8e3e4d43ae17dbe71adce
Thomas Tornevall <thorne@tornevall.net>
2026-08-26 11:29:33 +0200

    Clean up Statuspage data on uninstall

--- uninstall.php
+++ uninstall.php
@@ -10,5 +10,13 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 wp_clear_scheduled_hook( 'ttfw_dyndns_update_event' );
+
+$statuspage_settings = get_option( 'ttfw_statuspage_settings', array() );
+if ( is_array( $statuspage_settings ) && ! empty( $statuspage_settings['slug'] ) ) {
+	delete_transient( 'ttfw_statuspage_live_' . md5( (string) $statuspage_settings['slug'] ) );
+}
+
+delete_option( 'ttfw_statuspage_settings' );
+delete_option( 'ttfw_statuspage_last_good' );
 delete_option( 'ttfw_options' );
 delete_option( 'ttfw_dyndns_status' );
