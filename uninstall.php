1acca309c52d16d9d5ee75c8864bdf16309f95d6
Thomas Tornevall <thorne@tornevall.net>
2026-08-26 11:36:36 +0200

    Prefix Statuspage uninstall state

--- uninstall.php
+++ uninstall.php
@@ -11,9 +11,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 wp_clear_scheduled_hook( 'ttfw_dyndns_update_event' );
 
-$statuspage_settings = get_option( 'ttfw_statuspage_settings', array() );
-if ( is_array( $statuspage_settings ) && ! empty( $statuspage_settings['slug'] ) ) {
-	delete_transient( 'ttfw_statuspage_live_' . md5( (string) $statuspage_settings['slug'] ) );
+$ttfw_statuspage_settings = get_option( 'ttfw_statuspage_settings', array() );
+if ( is_array( $ttfw_statuspage_settings ) && ! empty( $ttfw_statuspage_settings['slug'] ) ) {
+	delete_transient( 'ttfw_statuspage_live_' . md5( (string) $ttfw_statuspage_settings['slug'] ) );
 }
 
 delete_option( 'ttfw_statuspage_settings' );
